8c36986cf3ad999976c5d26bd6a05e21968613c6
亮 杨 <1414877831@qq.com>
2018-01-07 10:13:50 +0800

    转字符串

---
+++ Arthmetic-yl/stdlib.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "stdlib.h"
+
+
+stdlib::stdlib()
+{
+}
+
+
+stdlib::~stdlib()
+{
+}
---
+++ Arthmetic-yl/stdlib.h
@@ -0,0 +1,8 @@
+#pragma once
+class stdlib
+{
+public:
+	stdlib();
+	~stdlib();
+};
+
